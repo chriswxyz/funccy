50465a0835ad3ce57bee9b85b2ed08741ab4d3ff
Chris Williams <chriswxyz@protonmail.com>
2017-10-14 15:48:25 -0500

    include log again

---
+++ src/Funccy/Log/Log.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Funccy
+{
+    /// <summary>
+    /// A type with an associated log.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class Log<T, TLog>
+    {
+        private IImmutableList<TLog> _log = ImmutableList<TLog>.Empty;
+        private readonly T _value;
+
+        /// <summary>
+        /// Creates a new Log.
+        /// </summary>
+        /// <param name="value"></param>
+        public Log(T value)
+        {
+            _value = value;
+        }
+
+        /// <summary>
+        /// Creates a new Log with history.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="log"></param>
+        public Log(T value, IImmutableList<TLog> log)
+        {
+            _value = value;
+            _log = log;
+        }
+        
+        /// <summary>
+        /// Maps a function onto the Log workflow.
+        /// </summary>
+        /// <typeparam name="TNext"></typeparam>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public Log<TNext, TLog> Map<TNext>(Func<T, TNext> f)
+        {
+            return new Log<TNext, TLog>(f(_value), _log);
+        }
+
+        /// <summary>
+        /// Adds a log message.
+        /// </summary>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public Log<T, TLog> Add(Func<T, TLog> f)
+        {
+            var next = _log.Add(f(_value));
+            return new Log<T, TLog>(_value, next);
+        }
+
+        /// <summary>
+        /// Gets the value and the log.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public (T, IImmutableList<TLog>) Extract()
+        {
+            return (_value, _log);
+        }
+    }
+}
---
+++ src/Funccy/Log/LogExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Funccy
+{
+    public static class LogExtensions
+    {
+        public static Log<T, TLog> AsLog<T, TLog>(this T obj)
+        {
+            return new Log<T, TLog>(obj);
+        }
+    }
+}
