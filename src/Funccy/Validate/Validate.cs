0a825d671367f28fd9a87eb9020b15070c8304cb
Chris Williams <chriswxyz@protonmail.com>
2018-09-04 20:31:17 -0500

    Remove problem model, just use defined generic type

--- src/Funccy/Validate/Validate.cs
+++ src/Funccy/Validate/Validate.cs
@@ -10,50 +10,48 @@ namespace Funccy
     {
         private class Rule
         {
-            public Rule(string key, Func<TData, Task<bool>> pred, Func<TData, TErr> desc)
+            public Rule(Func<TData, Task<bool>> pred, Func<TData, TErr> desc)
             {
-                Key = key;
                 Pred = pred;
                 Describe = desc;
             }
 
-            public string Key { get; }
             public Func<TData, Task<bool>> Pred { get; }
             public Func<TData, TErr> Describe { get; }
         }
 
         private IImmutableList<Rule> _rules = ImmutableList<Rule>.Empty;
 
-        public Validate<TData, TErr> WithRule(string key, Func<TData, bool> pred, Func<TData, TErr> description)
+        public Validate<TData, TErr> WithRule(Func<TData, bool> pred, Func<TData, TErr> description)
         {
-            var r = new Rule(key, x => Task.FromResult(pred(x)), description);
+            var r = new Rule(x => Task.FromResult(pred(x)), description);
             _rules = _rules.Add(r);
             return this;
         }
 
-        public Validate<TData, TErr> WithRule(string key, Func<TData, Task<bool>> pred, Func<TData, TErr> description)
+        public Validate<TData, TErr> WithRule(Func<TData, Task<bool>> pred, Func<TData, TErr> description)
         {
-            var r = new Rule(key, pred, description);
+            var r = new Rule(pred, description);
             _rules = _rules.Add(r);
             return this;
         }
 
-        public async Task<OneOf<TData, Problem<TErr>[]>> Check(TData model)
+        public async Task<OneOf<TData, TErr[]>> Check(TData model)
         {
             Task<bool> RulePassed(Rule r) => r.Pred(model);
 
             var failed = await _rules.WhereAsync(async r => !(await RulePassed(r)));
 
-            if (!failed.Any()) { return new OneOf<TData, Problem<TErr>[]>(model); }
+            if (!failed.Any()) { return new OneOf<TData, TErr[]>(model); }
 
             var problems = failed
-                .Select(x => new Problem<TErr>(x.Key, x.Describe(model)))
+                .Select(x => x.Describe(model))
                 .ToArray();
 
-            return new OneOf<TData, Problem<TErr>[]>(problems);
+            return new OneOf<TData, TErr[]>(problems);
         }
 
-        public Task<IEnumerable<OneOf<TData, Problem<TErr>[]>>> CheckAll(IEnumerable<TData> models)
+        public Task<IEnumerable<OneOf<TData, TErr[]>>> CheckAll(IEnumerable<TData> models)
         {
             return models.Select(Check).WhenAll();
         }
--- test/FunccyTests/ValidateTests.cs
+++ test/FunccyTests/ValidateTests.cs
@@ -11,58 +11,14 @@ namespace FunccyTests
         [TestMethod]
         public async Task Validate_Test()
         {
-            var validator = new Validate<string, string>()
-                .WithRule("noXs",
-                    x => !x.Any(c => c == 'x'),
-                    x => $"Has an x at position {x.IndexOf('x')}")
-                .WithRule("under20",
-                    x => x.Length < 20,
-                    x => $"Length was {x.Length}")
-                .WithRule("remoteValidation",
-                    x => CheckWithServer(x),
-                    x => "Remote service did not accept")
-                ;
-
-            var values = new[] {
-                "hello world",
-                "helloxx worldxx",
-                "it's your world, you can put whatever you want in it"
-            };
-
-            var results = (await validator
-                .CheckAll(values))
-                .Select(GetValueOrProblemSummary)
-                .ToArray();
-
-            Assert.AreEqual("hello world", results[0]);
-            Assert.AreEqual("Has an x at position 5", results[1]);
-            Assert.AreEqual("Length was 52, Remote service did not accept", results[2]);
-        }
-
-        public class ExampleErrorModel
-        {
-            public ExampleErrorModel(int code, string moreInfo)
-            {
-                Code = code;
-                MoreInfo = moreInfo;
-            }
-
-            public int Code { get; set; }
-            public string MoreInfo { get; set; }
-        }
-
-        [TestMethod]
-        public async Task Validate_CustomModels()
-        {
-
             var validator = new Validate<string, ExampleErrorModel>()
-                .WithRule("noXs",
+                .WithRule(
                     x => !x.Any(c => c == 'x'),
                     x => new ExampleErrorModel(100, $"Has an x at position {x.IndexOf('x')}"))
-                .WithRule("under20",
+                .WithRule(
                     x => x.Length < 20,
                     x => new ExampleErrorModel(102, $"Length was {x.Length}"))
-                .WithRule("remoteValidation",
+                .WithRule(
                     x => CheckWithServer(x),
                     x => new ExampleErrorModel(107, "Remote service did not accept"))
                 ;
@@ -83,26 +39,29 @@ namespace FunccyTests
             Assert.AreEqual("(102) Length was 52, (107) Remote service did not accept", results[2]);
         }
 
+        public class ExampleErrorModel
+        {
+            public ExampleErrorModel(int code, string moreInfo)
+            {
+                Code = code;
+                MoreInfo = moreInfo;
+            }
+
+            public int Code { get; set; }
+            public string MoreInfo { get; set; }
+        }
+
         public static Task<bool> CheckWithServer(string input)
         {
             // pretend to check something remotely
             return Task.FromResult(!input.Contains("whatever"));
         }
-
-        public static string GetValueOrProblemSummary(OneOf<string, Problem<string>[]> result)
-        {
-            return result.Extract(
-                    ok => ok,
-                    ps => ps.Select(x => x.Description).StringJoin(", ")
-                );
-        }
-
-        public static string GetValueOrProblemSummary(OneOf<string, Problem<ExampleErrorModel>[]> result)
+        
+        public static string GetValueOrProblemSummary(OneOf<string, ExampleErrorModel[]> result)
         {
             return result.Extract(
                     ok => ok,
                     ps => ps
-                        .Select(x => x.Description)
                         .Select(x => $"({x.Code}) {x.MoreInfo}")
                         .StringJoin(", ")
                 );
