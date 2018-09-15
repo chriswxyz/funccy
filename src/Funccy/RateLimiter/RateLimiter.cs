d9465ce2f0e1353e948b742e1cf01db7c8912869
Chris Williams <chriswxyz@protonmail.com>
2018-09-15 11:24:28 -0500

    Adds rate limiter and number extensions

---
+++ src/Funccy/RateLimiter/RateLimiter.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Funccy
+{
+    /// <summary>
+    /// Limits the amount of actions run in a certain time span.
+    /// </summary>
+    public class RateLimiter
+    {
+        // inspired by https://codereview.stackexchange.com/questions/87132/throttle-actions-by-number-per-period
+        private readonly TimeSpan _timeSpan;
+
+        // Some facts about SemaphoreSlim:
+        // 1. The constructor signature means (available resources, max resources)
+        // 2. They're IDisposable, but only the AvailableWaitHandle prop
+        // uses unmanaged resources, so there's no need to dispose the ones here.
+        // See https://stackoverflow.com/questions/32033416/do-i-need-to-dispose-a-semaphoreslim/39195920#39195920
+        // 3. They're runtime level, not OS level.
+        private readonly SemaphoreSlim _actions;
+        private readonly SemaphoreSlim _time;
+
+        public RateLimiter(int actions, TimeSpan timeSpan)
+        {
+            _actions = new SemaphoreSlim(actions, actions);
+            _time = new SemaphoreSlim(actions, actions);
+            _timeSpan = timeSpan;
+        }
+
+        public async Task<T> Run<T>(Func<T> action)
+        {
+            return await Run(action, CancellationToken.None);
+        }
+
+        public async Task<T> Run<T>(Func<T> action, CancellationToken cancel)
+        {
+            await _actions.WaitAsync(cancel);
+
+            try
+            {
+                await _time.WaitAsync(cancel);
+
+                await Task.Delay(_timeSpan);
+
+                _time.Release(1);
+
+                return action();
+            }
+            finally
+            {
+                _actions.Release(1);
+            }
+        }
+
+        public async Task<T> Run<T>(Func<Task<T>> action)
+        {
+            return await Run(action, CancellationToken.None);
+        }
+
+        public async Task<T> Run<T>(Func<Task<T>> action, CancellationToken cancel)
+        {
+            await _actions.WaitAsync(cancel);
+
+            try
+            {
+                await _time.WaitAsync(cancel);
+
+                await Task.Delay(_timeSpan);
+
+                _time.Release(1);
+
+                return await action();
+            }
+            finally
+            {
+                _actions.Release(1);
+            }
+        }
+    }
+
+    public static class LimiterExtensions
+    {
+        /// <summary>
+        /// Rate limits a collection mapping.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="arr"></param>
+        /// <param name="f"></param>
+        /// <param name="limiter"></param>
+        /// <returns></returns>
+        public static async Task<U[]> Select<T, U>(
+            this IEnumerable<T> arr,
+            Func<T, U> f,
+            RateLimiter limiter)
+        {
+            var tasks = arr.Select(x => limiter.Run(() => f(x)));
+
+            var results = await Task.WhenAll(tasks);
+
+            return results;
+        }
+
+        /// <summary>
+        /// Rate limits a collection mapping.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="arr"></param>
+        /// <param name="f"></param>
+        /// <param name="limiter"></param>
+        /// <returns></returns>
+        public static async Task<U[]> Select<T, U>(
+            this IEnumerable<T> arr,
+            Func<T, Task<U>> f,
+            RateLimiter limiter)
+        {
+            var tasks = arr.Select(x => limiter.Run(() => f(x)));
+
+            var results = await Task.WhenAll(tasks);
+
+            return results;
+        }
+    }
+}
---
+++ test/FunccyTests/RateLimiterTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Funccy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunccyTests
+{
+    [TestClass]
+    public class RateLimiterTests
+    {
+        [TestMethod]
+        public async Task RateLimiterLimitsRate()
+        {
+            var stopwatch = new Stopwatch();
+
+            var limiter = new RateLimiter(3, TimeSpan.FromSeconds(1));
+
+            stopwatch.Start();
+
+            var nums = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var results = await nums.Select(
+                x => stopwatch.ElapsedMilliseconds,
+                limiter);
+            
+            stopwatch.Stop();
+
+            Assert.IsTrue(results[0].IsNear(1000, 100));
+            Assert.IsTrue(results[1].IsNear(1000, 100));
+            Assert.IsTrue(results[2].IsNear(1000, 100));
+
+            Assert.IsTrue(results[3].IsNear(2000, 100));
+            Assert.IsTrue(results[4].IsNear(2000, 100));
+            Assert.IsTrue(results[5].IsNear(2000, 100));
+
+            Assert.IsTrue(results[6].IsNear(3000, 100));
+            Assert.IsTrue(results[7].IsNear(3000, 100));
+            Assert.IsTrue(results[8].IsNear(3000, 100));
+        }
+    }
+}
